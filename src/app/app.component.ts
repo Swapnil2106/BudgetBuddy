b34d5e263cbb290d73076603d4652023c4a17f5f
Swapnil2106 <swapnillkore@gmail.com>
2025-02-26 10:02:15 +0530

    added transaction component and some dummy data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponentComponent } from "./components/header-component/header-component.component";
 import { BalanceComponentComponent } from "./components/balance-component/balance-component.component";
+import { TransactionComponentComponent } from "./components/transaction-component/transaction-component.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponentComponent, BalanceComponentComponent],
+  imports: [RouterOutlet, HeaderComponentComponent, BalanceComponentComponent, TransactionComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
