0da4441f56dcc3936b463cc6ae133ea0ba494e32
Swapnil2106 <swapnillkore@gmail.com>
2025-02-28 15:25:27 +0530

    added routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { HeaderComponentComponent } from "./components/header-component/header-component.component";
-import { BalanceComponentComponent } from "./components/balance-component/balance-component.component";
-import { TransactionComponentComponent } from "./components/transaction-component/transaction-component.component";
-import { AddNewTransactionComponentComponent } from "./components/add-new-transaction-component/add-new-transaction-component.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponentComponent, BalanceComponentComponent, TransactionComponentComponent, AddNewTransactionComponentComponent],
+  imports: [RouterOutlet, HeaderComponentComponent, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/budget-component/budget-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-budget-component',
+  imports: [],
+  templateUrl: './budget-component.component.html',
+  styleUrl: './budget-component.component.css'
+})
+export class BudgetComponentComponent {
+
+}
